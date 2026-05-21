1617dfb37835f7ff3a9d0365925fac2f08a63b88
chandan kumar <kailashpramila1234@gmail.com>
2026-05-21 21:43:12 +0530

    21-05-2026 level-0

---
+++ Level0/01_Details_program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Name : Dinesh" << endl;
+    cout << "Age  : 20" << endl;
+    cout << "City : Bangalore" << endl;
+
+    return 0;
+}
